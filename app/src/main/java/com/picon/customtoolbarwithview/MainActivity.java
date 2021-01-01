93980563101dcdf0e9e47a84c8d0052000883d9f
mohiuddin <mohiuddin655.bd@gmail.com>
2021-01-01 23:01:18 +0600

    add custom toolbar layout

--- app/src/main/java/com/picon/customtoolbarwithview/MainActivity.java
+++ app/src/main/java/com/picon/customtoolbarwithview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.picon.customtoolbarwithview;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.layout_action_bar);
     }
 }
