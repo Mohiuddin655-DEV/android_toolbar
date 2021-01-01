f4a455892676dea917a259966a9aa16b72c3f219
mohiuddin <mohiuddin655.bd@gmail.com>
2021-01-01 23:32:08 +0600

    add toolbar icon and listener.

--- app/src/main/java/com/picon/customtoolbarwithview/MainActivity.java
+++ app/src/main/java/com/picon/customtoolbarwithview/MainActivity.java
@@ -3,6 +3,9 @@ package com.picon.customtoolbarwithview;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.layout_action_bar);
+
+        View view = getSupportActionBar().getCustomView();
+
+        ImageButton imageButton = view.findViewById(R.id.action_bar_message);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hi, I'm here...!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
